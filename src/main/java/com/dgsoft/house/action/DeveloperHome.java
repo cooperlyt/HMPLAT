d0cfe17c7230f0465bc33206b679a2c40ea34968
cooper-lyt <lyt3131@163.com>
2014-06-12 15:12:14 +0800

    developer

--- src/main/java/com/dgsoft/house/action/DeveloperHome.java
+++ src/main/java/com/dgsoft/house/action/DeveloperHome.java
@@ -2,7 +2,10 @@ package com.dgsoft.house.action;
 
 import com.dgsoft.house.HouseSimpleEntityHome;
 import com.dgsoft.house.model.Developer;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.international.StatusMessage;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,7 +16,17 @@ import org.jboss.seam.annotations.Name;
 @Name("developerHome")
 public class DeveloperHome extends HouseSimpleEntityHome<Developer> {
 
+    @In
+    private FacesMessages facesMessages;
 
-
+    @Override
+    protected boolean verifyRemoveAvailable(){
+        if (getEntityManager().createQuery("select coalesce(Count(project.id),0) from Project project where project.developer.id = :developerId",Long.class)
+                .setParameter("developerId",getInstance().getId()).getSingleResult() > 0){
+            facesMessages.addFromResourceBundle(StatusMessage.Severity.ERROR,"Developer_cant_remove");
+            return false;
+        }
+        return true;
+    }
 
 }
