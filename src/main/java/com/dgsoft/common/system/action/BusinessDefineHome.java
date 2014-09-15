400e879023b5268ece3a53e2a439f226a48d6ac3
cooper <lyt3131@163.com>
2014-09-15 15:56:03 +0800

    business begin

--- src/main/java/com/dgsoft/common/system/action/BusinessDefineHome.java
+++ src/main/java/com/dgsoft/common/system/action/BusinessDefineHome.java
@@ -32,9 +32,9 @@ public class BusinessDefineHome extends SystemEntityHome<BusinessDefine> {
     @In
     private FacesMessages facesMessages;
 
-    public List<TaskSubscribe> getTaskSubscribeList(){
+    public List<TaskSubscribe> getTaskSubscribeList() {
         List<TaskSubscribe> result = new ArrayList<TaskSubscribe>(getInstance().getTaskSubscribes());
-        Collections.sort(result,new Comparator<TaskSubscribe>() {
+        Collections.sort(result, new Comparator<TaskSubscribe>() {
             @Override
             public int compare(TaskSubscribe o1, TaskSubscribe o2) {
                 return new Integer(o1.getPriority()).compareTo(o2.getPriority());
@@ -46,17 +46,17 @@ public class BusinessDefineHome extends SystemEntityHome<BusinessDefine> {
     @In
     private ActionExecuteState actionExecuteState;
 
-    @Factory(value = "taskSubscribeTypes",scope = ScopeType.SESSION)
-    public TaskSubscribe.SubscribeType[] getTaskSubscribeTypes(){
+    @Factory(value = "taskSubscribeTypes", scope = ScopeType.SESSION)
+    public TaskSubscribe.SubscribeType[] getTaskSubscribeTypes() {
         return TaskSubscribe.SubscribeType.values();
     }
 
-    private List<TotalDataGroup<String,TaskSubscribe>> taskSubscribeGroups;
+    private List<TotalDataGroup<String, TaskSubscribe>> taskSubscribeGroups;
 
 
     public List<TotalDataGroup<String, TaskSubscribe>> getTaskSubscribeGroups() {
-        if (taskSubscribeGroups == null){
-            taskSubscribeGroups = TotalDataGroup.groupBy(getInstance().getTaskSubscribes(),new TotalGroupStrategy<String, TaskSubscribe>() {
+        if (taskSubscribeGroups == null) {
+            taskSubscribeGroups = TotalDataGroup.groupBy(getInstance().getTaskSubscribes(), new TotalGroupStrategy<String, TaskSubscribe>() {
                 @Override
                 public String getKey(TaskSubscribe taskSubscribe) {
                     return taskSubscribe.getTaskName();
@@ -67,11 +67,25 @@ public class BusinessDefineHome extends SystemEntityHome<BusinessDefine> {
                     return null;
                 }
             });
+            sortSubscribeGroups();
         }
         return taskSubscribeGroups;
     }
 
-    public List<String> getWfTaskNames(){
+    private void sortSubscribeGroups() {
+        if (taskSubscribeGroups != null)
+            for (TotalDataGroup<String, TaskSubscribe> group : taskSubscribeGroups) {
+                TotalDataGroup.sort(group, new Comparator<TaskSubscribe>() {
+                    @Override
+                    public int compare(TaskSubscribe o1, TaskSubscribe o2) {
+                        return Integer.valueOf(o1.getPriority()).compareTo(o2.getPriority());
+                    }
+                });
+            }
+    }
+
+
+    public List<String> getWfTaskNames() {
         ProcessDefinition lasterPD = ManagedJbpmContext.instance().getGraphSession().findLatestProcessDefinition(getInstance().getWfName());
 
         return new ArrayList<String>(lasterPD.getTaskMgmtDefinition().getTasks().keySet());
@@ -80,10 +94,12 @@ public class BusinessDefineHome extends SystemEntityHome<BusinessDefine> {
 
     private TaskSubscribe editTaskSubscribe;
 
-    public boolean isHaveNeedFile(){
+    public boolean isHaveNeedFile() {
         //TODO need file
         return false;
-    };
+    }
+
+    ;
 
     public TaskSubscribe getEditTaskSubscribe() {
         return editTaskSubscribe;
@@ -94,9 +110,9 @@ public class BusinessDefineHome extends SystemEntityHome<BusinessDefine> {
     }
 
 
-    public void setSelectTaskSubscribeId(String id){
-        for(TaskSubscribe ts: getInstance().getTaskSubscribes()){
-            if (ts.getId().equals(id)){
+    public void setSelectTaskSubscribeId(String id) {
+        for (TaskSubscribe ts : getInstance().getTaskSubscribes()) {
+            if (ts.getId().equals(id)) {
                 editTaskSubscribe = ts;
                 return;
             }
@@ -104,8 +120,8 @@ public class BusinessDefineHome extends SystemEntityHome<BusinessDefine> {
         editTaskSubscribe = null;
     }
 
-    public String getSelectTaskSubscribeId(){
-        if (editTaskSubscribe == null){
+    public String getSelectTaskSubscribeId() {
+        if (editTaskSubscribe == null) {
             return null;
         }
         return editTaskSubscribe.getId();
@@ -121,32 +137,90 @@ public class BusinessDefineHome extends SystemEntityHome<BusinessDefine> {
         this.createing = createing;
     }
 
-    public void createTaskSubscribe(){
+    public void createTaskSubscribe() {
         createing = true;
         actionExecuteState.clearState();
-        editTaskSubscribe = new TaskSubscribe(UUID.randomUUID().toString().replace("-", "").toUpperCase(),getInstance());
+        editTaskSubscribe = new TaskSubscribe(UUID.randomUUID().toString().replace("-", "").toUpperCase(), getInstance());
     }
 
-    public void editTaskSubscribe(){
+    public void editTaskSubscribe() {
         createing = false;
         actionExecuteState.clearState();
     }
 
-    public void saveTaskSubscribe(){
+    public void saveTaskSubscribe() {
         if (createing) {
+
             getInstance().getTaskSubscribes().add(editTaskSubscribe);
             taskSubscribeGroups = null;
             if (editTaskSubscribe.getType().equals(TaskSubscribe.SubscribeType.START_TASK)) {
                 editTaskSubscribe.setTaskName("start");
             }
+            editTaskSubscribe.setPriority(getMaxPriority(editTaskSubscribe.getTaskName()) + 1);
         }
         actionExecuteState.actionExecute();
+    }
 
+    private int getMaxPriority(String taskName) {
+        int result = 0;
+        for (TaskSubscribe subscribe : getInstance().getTaskSubscribes()) {
+            if (taskName.equals(subscribe.getTaskName()) && (subscribe.getPriority() > result)) {
+                result = subscribe.getPriority();
+            }
+        }
+        return result;
+    }
+
+    public void upSelectTaskSubscribe() {
+        int selectPriority = editTaskSubscribe.getPriority();
+
+        //Integer maxPriority = null;
+
+        TaskSubscribe maxSub = null;
+        for (TaskSubscribe subscribe : getInstance().getTaskSubscribes()) {
+            if (editTaskSubscribe.getTaskName().equals(subscribe.getTaskName()) && (subscribe.getPriority() < selectPriority)) {
+                if ((maxSub == null) || (maxSub.getPriority() < subscribe.getPriority())){
+                    maxSub = subscribe;
+                }
+                //subscribe.setPriority(subscribe.getPriority() - 1);
+            }
+        }
+
+        if (maxSub != null){
+            int maxPriority = maxSub.getPriority();
+            maxSub.setPriority(editTaskSubscribe.getPriority());
+            editTaskSubscribe.setPriority(maxPriority);
+            sortSubscribeGroups();
+        }
+
+    }
+
+    public void downSelectTaskSubscribe() {
+        int selectPriority = editTaskSubscribe.getPriority();
+        TaskSubscribe minSub = null;
+        for (TaskSubscribe subscribe : getInstance().getTaskSubscribes()) {
+            if (editTaskSubscribe.getTaskName().equals(subscribe.getTaskName()) && (subscribe.getPriority() > selectPriority)) {
+                if ((minSub == null) || (minSub.getPriority() > subscribe.getPriority())){
+                    minSub = subscribe;
+                }
+            }
+        }
+        if (minSub != null){
+            int minPriority = minSub.getPriority();
+            minSub.setPriority(editTaskSubscribe.getPriority());
+            editTaskSubscribe.setPriority(minPriority);
+            sortSubscribeGroups();
+        }
+    }
+
+    public void deleteSelectSubscribe(){
+        getInstance().getTaskSubscribes().remove(editTaskSubscribe);
+        taskSubscribeGroups = null;
     }
 
 
     @Override
-    public void refresh(){
+    public void refresh() {
         super.refresh();
         taskSubscribeGroups = null;
     }
