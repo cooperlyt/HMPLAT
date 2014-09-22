d67dfceeb623096c2c0129fb0d0eb1727cb8a2e6
23504653 <26982912@qq.com>
2014-09-22 15:49:42 +0800

    wxy

--- src/main/java/com/dgsoft/house/owner/business/subscribe/BaseBusinessPersionSubscribe.java
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/BaseBusinessPersionSubscribe.java
@@ -2,6 +2,7 @@ package com.dgsoft.house.owner.business.subscribe;
 
 import com.dgsoft.common.system.PersonEntityHomeHelper;
 import com.dgsoft.house.owner.OwnerEntityHome;
+import com.dgsoft.house.owner.action.OwnerBusinessHome;
 import com.dgsoft.house.owner.model.BusinessHouseOwner;
 import com.dgsoft.house.owner.model.BusinessPersion;
 import org.jboss.seam.annotations.In;
@@ -14,13 +15,19 @@ import org.jboss.seam.annotations.In;
  * To change this template use File | Settings | File Templates.
  */
 
-public class BaseBusinessPersionSubscribe extends OwnerEntityHome<BusinessPersion> {
+public abstract class BaseBusinessPersionSubscribe extends OwnerEntityHome<BusinessPersion> {
+
+    @In
+    private OwnerBusinessHome ownerBusinessHome;
+
+    protected abstract BusinessPersion.PersionType getType();
 
     private PersonEntityHomeHelper<BusinessPersion> persionPersonEntityHomeHelper;
 
     public PersonEntityHomeHelper<BusinessPersion> getPersionPersonEntityHomeHelper() {
         return persionPersonEntityHomeHelper;
     }
+
 
     @Override
     public Class<BusinessPersion> getEntityClass() {
@@ -31,5 +38,19 @@ public class BaseBusinessPersionSubscribe extends OwnerEntityHome<BusinessPersio
     public void create(){
         super.create();
         persionPersonEntityHomeHelper = new PersonEntityHomeHelper<BusinessPersion>(this);
+        for(BusinessPersion businessPersion:ownerBusinessHome.getInstance().getBusinessPersions()){
+            if(businessPersion.getType().equals(getType())){
+                setId(businessPersion.getId());
+                return;
+            }
+        }
+        getInstance().setOwnerBusiness(ownerBusinessHome.getInstance());
+        ownerBusinessHome.getInstance().getBusinessPersions().add(getInstance());
     }
+
+    @Override
+    protected BusinessPersion createInstance() {
+        return new BusinessPersion(getType());
+    }
+
 }
