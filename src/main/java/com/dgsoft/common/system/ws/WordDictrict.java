1c15f8b50ad94b641cb6af19edc84e36d32a135f
cooper <lyt3131@163.com>
2014-09-23 11:11:18 +0800

    autoView

--- src/main/java/com/dgsoft/common/system/ws/WordDictrict.java
+++ src/main/java/com/dgsoft/common/system/ws/WordDictrict.java
@@ -4,6 +4,7 @@ import com.dgsoft.common.system.DictionaryWord;
 import com.dgsoft.common.system.model.Word;
 import com.dgsoft.common.system.model.WordCategory;
 import org.jboss.seam.Component;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,16 +26,19 @@ public class WordDictrict {
 
             for (WordCategory category : dictionary.getAllWordCategory().values()) {
 
-                JSONObject jsonObject = new JSONObject();
+                JSONArray jsonArray = new JSONArray();
+
                 for (Word word : dictionary.getWordList(category.getId())) {
+                    JSONObject jsonObject = new JSONObject();
                     jsonObject.put("ID", word.getId());
                     jsonObject.put("VALUE", word.getValue());
                     jsonObject.put("KEY", word.getKey());
                     jsonObject.put("DESCRIPTION", word.getDescription());
                     jsonObject.put("PRIORITY", word.getPriority());
+                    jsonArray.put(jsonObject);
                 }
 
-                categoryOjb.put(category.getId(), jsonObject);
+                categoryOjb.put(category.getId(), jsonArray);
 
             }
 
