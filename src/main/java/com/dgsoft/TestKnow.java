1a616ba88a9da69381058a0dd070a40bba1edb35
cooper <lyt3131@163.com>
2014-11-24 11:14:40 +0800

    autoView

--- src/main/java/com/dgsoft/TestKnow.java
+++ src/main/java/com/dgsoft/TestKnow.java
@@ -12,6 +12,8 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -23,20 +25,90 @@ import java.util.Locale;
 @Name("testKnow")
 public class TestKnow {
 
+    public List<TopLevel> getTopLevelDatas(){
+        List<TopLevel> result = new ArrayList<TopLevel>(1);
+        result.add(new TopLevel());
+        return result;
+    }
+
+    public static class TopLevel {
+
+        private String title;
+
+        private List<SecondLevel> secondLevels = new ArrayList<SecondLevel>(1);
+
+        public TopLevel() {
+            this.title = "test Top level";
+            secondLevels.add(new SecondLevel("test second Level"));
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public List<SecondLevel> getSecondLevels() {
+            return secondLevels;
+        }
+
+        public void setSecondLevels(List<SecondLevel> secondLevels) {
+            this.secondLevels = secondLevels;
+        }
+    }
+
+    public static class SecondLevel {
+        private String title;
+
+        private boolean expand;
+
+        private List<String> thirdLevels = new ArrayList<String>(15);
+
+        public SecondLevel(String title) {
+            this.title = title;
+            for (int i = 0; i < 15; i++) {
+                thirdLevels.add(String.valueOf(i));
+            }
+            expand = false;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public List<String> getThirdLevels() {
+            return thirdLevels;
+        }
+
+        public void setThirdLevels(List<String> thirdLevels) {
+            this.thirdLevels = thirdLevels;
+        }
+
+        public boolean isExpand() {
+            return expand;
+        }
+
+        public void setExpand(boolean expand) {
+            this.expand = expand;
+        }
+    }
 
 
-    public static class TestAnn{
+    public static class TestAnn {
 
 
         private String name;
 
 
-
         public String getName() {
             return name;
         }
-
-
 
 
         public void setName(String name) {
@@ -45,16 +117,16 @@ public class TestKnow {
     }
 
 
-    public static void main(String[] args){
-       //System.out.println(GBT.getGB17710("1101010102006050001090001") );
-       // System.out.println(GBT.getGB17710("11010800000001") );
+    public static void main(String[] args) {
+        //System.out.println(GBT.getGB17710("1101010102006050001090001") );
+        // System.out.println(GBT.getGB17710("11010800000001") );
 //        System.out.println(GBT.formatCode("123456789", 4) );
 //        System.out.println(GBT.formatCode("3", 4) );
 //        System.out.println(GBT.formatCode("1234", 4) );
 
 //        javax.faces.component.html.HtmlPanelGrid
 //        javax.faces.component.html.
-        System.out.println("1234567890".substring(0,5));
+        System.out.println("1234567890".substring(0, 5));
 
     }
 
@@ -65,17 +137,18 @@ public class TestKnow {
     @Logger
     private org.jboss.seam.log.Log log;
 
-    public void printPageContext(){
+    public void printPageContext() {
         log.debug("print page Context-------------");
-       for(String name:Contexts.getPageContext().getNames()){
+        for (String name : Contexts.getPageContext().getNames()) {
 
 
-          log.debug(name + ":" + Contexts.getPageContext().get(name));
-       }
+            log.debug(name + ":" + Contexts.getPageContext().get(name));
+        }
 
 
-        org.jboss.seam.faces.FacesPage fp = (FacesPage) Contexts.getPageContext().get("org.jboss.seam.faces.facesPage");;
-        log.debug("page context convertID:" +  fp.getConversationId());
+        org.jboss.seam.faces.FacesPage fp = (FacesPage) Contexts.getPageContext().get("org.jboss.seam.faces.facesPage");
+        ;
+        log.debug("page context convertID:" + fp.getConversationId());
 
         log.debug("print page Context end-------------");
     }
@@ -90,7 +163,7 @@ public class TestKnow {
 
     public String getV2() {
 
-       return v2;
+        return v2;
     }
 
     public void setV2(String v2) {
