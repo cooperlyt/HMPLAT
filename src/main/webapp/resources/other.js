d26e76d2dc120e3a91f52d6a4ddbfeda49eb7d8a
cooper <lyt3131@163.com>
2014-12-17 17:05:04 +0800

    page change

--- src/main/webapp/resources/other.js
+++ src/main/webapp/resources/other.js
@@ -4,6 +4,25 @@ $(document).ready(
     function(){
         $('.selectpicker').selectpicker();
         $('.select-menu-header').click(function(event){ if(!$(event.target).hasClass("js-menu-close")) event.stopPropagation();});
+
+        $('.modal').on('show.bs.modal', function (e) {
+
+            $('.form-group',document.getElementById(e.target.id)).removeClass('has-success');
+            $('.form-group',document.getElementById(e.target.id)).removeClass('has-error');
+            $('.js-form-input',document.getElementById(e.target.id)).removeClass('edit-valid');
+            $('.js-form-input',document.getElementById(e.target.id)).removeClass('edit-success');
+            $('.js-form-input',document.getElementById(e.target.id)).removeClass('edit-error');
+        })
+        initEditInput();
     }
 );
+
+function initEditInput(){
+    $('.form-group').removeClass('has-success');
+    $('.form-group').removeClass('has-error');
+    $('.js-form-input').removeClass('edit-valid');
+    $('.js-form-input').removeClass('edit-success');
+    $('.js-form-input').removeClass('edit-error');
+}
+
 
