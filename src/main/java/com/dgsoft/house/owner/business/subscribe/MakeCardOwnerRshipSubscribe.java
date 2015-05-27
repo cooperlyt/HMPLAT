1d8a860e4f877e29c76ad197ba365cedc4a2f11a
Administrator <26982912@qq.com>
2015-05-27 15:04:54 +0800

    wxy

---
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/MakeCardOwnerRshipSubscribe.java
@@ -0,0 +1,50 @@
+package com.dgsoft.house.owner.business.subscribe;
+
+import com.dgsoft.house.owner.OwnerEntityHome;
+import com.dgsoft.house.owner.action.OwnerBusinessHome;
+import com.dgsoft.house.owner.action.OwnerNumberBuilder;
+import com.dgsoft.house.owner.model.MakeCard;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+/**
+ * Created by Administrator on 15-5-27.
+ */
+@Name("makeCardOwnerRshipSubscribe")
+public class MakeCardOwnerRshipSubscribe extends OwnerEntityHome<MakeCard> {
+
+   @In
+   private OwnerBusinessHome ownerBusinessHome;
+
+
+   @In(create = true)
+   private OwnerNumberBuilder ownerNumberBuilder;
+
+
+  @Override
+  public MakeCard createInstance(){
+
+      return new MakeCard(MakeCard.CardType.NOTICE,false,ownerNumberBuilder.useDayNumber("NOTICE"));
+
+  }
+  @Override
+  public void create(){
+      super.create();
+      if (!ownerBusinessHome.getInstance().getMakeCards().isEmpty()){
+          setId(ownerBusinessHome.getInstance().getMakeCards().iterator().next().getId());
+      }else{
+          getInstance().setOwnerBusiness(ownerBusinessHome.getInstance());
+          ownerBusinessHome.getInstance().getMakeCards().add(getInstance());
+      }
+
+
+  }
+
+
+
+
+
+
+
+
+}
