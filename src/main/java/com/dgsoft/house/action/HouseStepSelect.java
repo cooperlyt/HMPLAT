a388ee6d7b31c661242cde6580062d7f715c8f6d
cooper <lyt3131@163.com>
2015-06-11 17:42:20 +0800

    New changelist

--- src/main/java/com/dgsoft/house/action/HouseStepSelect.java
+++ src/main/java/com/dgsoft/house/action/HouseStepSelect.java
@@ -1,15 +1,11 @@
 package com.dgsoft.house.action;
 
 import com.dgsoft.house.HouseEntityLoader;
-import com.dgsoft.house.model.District;
-import com.dgsoft.house.model.Project;
+import com.dgsoft.house.model.Build;
 import com.dgsoft.house.model.Section;
-import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Scope;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,157 +13,71 @@ import java.util.List;
  */
 
 @Name("houseStepSelect")
-@Scope(ScopeType.CONVERSATION)
 public class HouseStepSelect {
-
-    @In(create = true)
-    private BuildHome buildHome;
-
-    private District district;
-
-    private Section section;
-
-    private Project project;
-
-    private String houseDisplayCode;
-
-    private String mapCode;
-
-    private String blockCode;
-
-    private String buildCode;
-
-    private String houseOrder;
-
-    private String sectionName;
-
-    private List<Project> projects;
-
-    private static final String PROJECTS_DISTRICT_SQL = "select section.name from Section section where section.district.id = :districtId and  ((lower(section.pyCode) like lower(concat('%',:prefix,'%'))) or (lower(section.name) like lower(concat('%',:prefix,'%'))))";
-
-    private static final String PROJECTS_ALL_SQL = "select section.name from Section section where ((lower(section.pyCode) like lower(concat('%',:prefix,'%'))) or (lower(section.name) like lower(concat('%',:prefix,'%'))))";
-
 
     @In(create = true)
     private HouseEntityLoader houseEntityLoader;
 
+    private Section selectSection;
 
-    public District getDistrict() {
-        return district;
+    private String selectProjectId;
+
+    private String selectBuildId;
+
+    public Section getSelectSection() {
+        return selectSection;
     }
 
-    public void setDistrict(District district) {
-        if ((district == null) || !district.equals(this.district)) {
-            setSection(null);
-            setSectionName(null);
+    public String getSelectSectionId(){
+        if (selectSection == null){
+            return null;
         }
-        this.district = district;
+        return selectSection.getId();
     }
 
-    public String getSectionName() {
-        return sectionName;
-    }
-
-    public void setSectionName(String sectionName) {
-        if ((sectionName == null) || sectionName.equals("") || (!sectionName.equals(this.sectionName))){
-            projects = null;
-            setProject(null);
-        }
-        this.sectionName = sectionName;
-    }
-
-    public List<Project> getProjects() {
-        if(projects == null){
-            if (section == null) {
-                projects = houseEntityLoader.getEntityManager().createQuery("select project from Project project where ((lower(project.section.pyCode) like lower(concat('%',:prefix,'%'))) or (lower(project.section.name) like lower(concat('%',:prefix,'%'))))", Project.class).
-                        setParameter("prefix", sectionName).getResultList();
-            }else{
-                projects = section.getProjectList();
-            }
-        }
-        return projects;
-    }
-
-    public List<String> sectionNameAutoComplete(String prefix){
-        if (getDistrict() != null){
-           return houseEntityLoader.getEntityManager().createQuery(PROJECTS_DISTRICT_SQL,String.class).
-                setParameter("districtId", getDistrict().getId()).setParameter("prefix",prefix).getResultList();
+    public void setSelectSectionId(String id){
+        if ((id == null) && id.trim().equals("")){
+            selectSection = null;
         }else{
-           return houseEntityLoader.getEntityManager().createQuery(PROJECTS_ALL_SQL,String.class).
-                setParameter("prefix", prefix).getResultList();
+            selectSection = houseEntityLoader.getEntityManager().find(Section.class,id);
         }
     }
 
-    public Project getProject() {
-        return project;
+    public String getSelectProjectId() {
+        return selectProjectId;
     }
 
-    public void setProject(Project project) {
-        if ((project == null) || !project.equals(this.project)) {
-            buildHome.clearInstance();
-        }
-        this.project = project;
+    public void setSelectProjectId(String selectProjectId) {
+        this.selectProjectId = selectProjectId;
     }
 
-    public List<Section> sectionAutoComplete(String prefix) {
-        if (district != null) {
-            return houseEntityLoader.getEntityManager().createQuery("select section from Section section where section.district.id = :districtId and  ((lower(section.pyCode) like lower(concat('%',:prefix,'%'))) or (lower(section.name) like lower(concat('%',:prefix,'%')))) ", Section.class).
-                    setParameter("prefix", prefix).setParameter("districtId",district.getId()).getResultList();
-        }else{
-            return new ArrayList<Section>(0);
-        }
+    public String getSelectBuildId() {
+        return selectBuildId;
     }
 
-    public Section getSection() {
-        return section;
+    public void setSelectBuildId(String selectBuildId) {
+        this.selectBuildId = selectBuildId;
     }
 
-    public void setSection(Section section) {
-        if ((section == null) || !section.equals(this.section)) {
-            projects = null;
-            setProject(null);
-        }
+    public void resetSection(){
+        selectSection = null;
+        resetPorject();
 
-        this.section = section;
     }
 
-    public String getHouseDisplayCode() {
-        return houseDisplayCode;
+    public void resetPorject(){
+        selectProjectId = null;
+        resetBuild();
     }
 
-    public void setHouseDisplayCode(String houseDisplayCode) {
-        this.houseDisplayCode = houseDisplayCode;
+    public void resetBuild(){
+
     }
 
-    public String getMapCode() {
-        return mapCode;
+
+    public List<Build> getBuildList(){
+       return houseEntityLoader.getEntityManager().createQuery("select build from Build build where build.project.id = :projectId order by build.buildNo ").setParameter("projectId", getSelectProjectId()).getResultList();
     }
 
-    public void setMapCode(String mapCode) {
-        this.mapCode = mapCode;
-    }
 
-    public String getBlockCode() {
-        return blockCode;
-    }
-
-    public void setBlockCode(String blockCode) {
-        this.blockCode = blockCode;
-    }
-
-    public String getBuildCode() {
-        return buildCode;
-    }
-
-    public void setBuildCode(String buildCode) {
-        this.buildCode = buildCode;
-    }
-
-    public String getHouseOrder() {
-        return houseOrder;
-    }
-
-    public void setHouseOrder(String houseOrder) {
-        this.houseOrder = houseOrder;
-    }
 }
---
+++ src/main/java/com/dgsoft/house/action/SectionSelectList.java
@@ -0,0 +1,78 @@
+package com.dgsoft.house.action;
+
+import com.dgsoft.house.HouseEntityQuery;
+import com.dgsoft.house.model.Section;
+import org.jboss.seam.annotations.Name;
+
+import javax.persistence.TypedQuery;
+import java.util.Arrays;
+
+/**
+ * Created by cooper on 6/11/15.
+ */
+@Name("sectionSelectList")
+public class SectionSelectList extends HouseEntityQuery<Section> {
+
+    private static final String EJBQL = "select section from Section section";
+
+    private static final int PAGE_COUNT = 12;
+
+
+    public SectionSelectList() {
+        setEjbql(EJBQL);
+
+        setOrderColumn("section.createTime");
+        setOrderDirection("desc");
+        setMaxResults(PAGE_COUNT);
+    }
+
+    public void moreResult(){
+        if (getMaxResults() == null){
+            setMaxResults(PAGE_COUNT);
+        }else{
+            setMaxResults(getMaxResults() + PAGE_COUNT);
+        }
+    }
+
+    protected void setSql(){
+        String sql = "";
+        if ((getDistrictId() != null ) && !getDistrictId().trim().equals("")) {
+            sql += " where section.district.id = '" + getDistrictId().trim() + "' ";
+        }
+        if ((getSearchKey() != null) && !getSearchKey().trim().equals("")){
+            if ("".equals(sql)){
+                sql = " where ";
+            }else {
+                sql += " and ";
+            }
+
+            sql += "( (lower(section.pyCode) like lower(concat('%','" + getSearchKey().trim()
+                    + "','%')) ) or (lower(section.name) like lower(concat('%','" + getSearchKey().trim() +"','%'))))";
+        }
+        setEjbql(EJBQL + sql);
+        setMaxResults(PAGE_COUNT);
+
+    }
+
+    private String districtId;
+
+    private String searchKey;
+
+    public String getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(String districtId) {
+        this.districtId = districtId;
+        setSql();
+    }
+
+    public String getSearchKey() {
+        return searchKey;
+    }
+
+    public void setSearchKey(String searchKey) {
+        this.searchKey = searchKey;
+        setSql();
+    }
+}
