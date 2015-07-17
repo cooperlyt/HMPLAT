60bc826444dab28f60ee88b08401333d022c6ab6
cooper <lyt3131@163.com>
2015-07-17 16:59:38 +0800

    Person complete

--- src/main/java/com/dgsoft/house/owner/business/validation/HouseStatusNotHaveSaleRegister.java
+++ src/main/java/com/dgsoft/house/owner/business/validation/HouseStatusNotHaveSaleRegister.java
@@ -1,6 +1,5 @@
 package com.dgsoft.house.owner.business.validation;
 
-import com.dgsoft.common.system.business.BusinessDataValid;
 import com.dgsoft.common.system.business.TaskSubscribeComponent;
 import com.dgsoft.house.HouseInfo;
 import com.dgsoft.house.owner.model.BusinessHouse;
@@ -13,10 +12,10 @@ import org.jboss.seam.annotations.intercept.BypassInterceptors;
  * Created by Administrator on 15-7-17.
  * 不能有预购商品房预告登记
  */
-@Name("houseStatusNotHaveSaleRegistr")
+@Name("houseStatusNotHaveSaleRegister")
 @Scope(ScopeType.STATELESS)
 @BypassInterceptors
-public class HouseStatusNotHaveSaleRegistr extends BusinessHouseValid {
+public class HouseStatusNotHaveSaleRegister extends BusinessHouseValid {
     @Override
     public ValidResult valid(BusinessHouse businessHouse) {
         if (businessHouse.getHouseStates().contains(HouseInfo.HouseStatus.SALE_REGISTER)){
