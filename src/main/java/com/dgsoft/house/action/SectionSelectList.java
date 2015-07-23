ed84c9afc6fbdf117dc056f2430432d3d2a099bd
cooper <lyt3131@163.com>
2015-07-23 16:02:50 +0800

    subscription interfaces change

--- src/main/java/com/dgsoft/house/action/SectionSelectList.java
+++ src/main/java/com/dgsoft/house/action/SectionSelectList.java
@@ -1,32 +1,56 @@
 package com.dgsoft.house.action;
 
+import com.dgsoft.common.utils.seam.MultiOperatorEntityQuery;
+import com.dgsoft.common.utils.seam.RestrictionGroup;
+import com.dgsoft.common.utils.seam.TestEntityQuery;
 import com.dgsoft.house.HouseEntityQuery;
 import com.dgsoft.house.model.Section;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.framework.EntityQuery;
+import org.jboss.seam.log.Logging;
+import org.json.Test;
 
 import javax.persistence.TypedQuery;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by cooper on 6/11/15.
  */
 @Name("sectionSelectList")
 @Scope(ScopeType.CONVERSATION)
-public class SectionSelectList extends HouseEntityQuery<Section> {
+public class SectionSelectList extends TestEntityQuery<Section> {
 
     private static final String EJBQL = "select section from Section section";
 
     private static final int PAGE_COUNT = 12;
 
+    private static final String[] RESTRICTIONS1 = {
+            "lower(section.district.id) = lower(#{sectionSelectList.districtId})",
+
+    };
+
+    private static final String[] RESTRICTIONS2 ={
+            "(lower(section.pyCode) like lower(concat('%',#{sectionSelectList.searchKey},'%')) ) " ,
+            " (lower(section.name) like lower(concat('%','#{sectionSelectList.searchKey}','%')))"
+    };
 
     public SectionSelectList() {
         setEjbql(EJBQL);
 
         setOrderColumn("section.createTime");
         setOrderDirection("desc");
+
+        setRestrictionLogicOperator("and");
+
+        RestrictionGroup districtRestriction = new RestrictionGroup("and",Arrays.asList(RESTRICTIONS1));
+
+        districtRestriction.getChildren().add(new RestrictionGroup("or",Arrays.asList(RESTRICTIONS2)));
+
         setMaxResults(PAGE_COUNT);
+
     }
 
     public void moreResult(){
@@ -34,27 +58,8 @@ public class SectionSelectList extends HouseEntityQuery<Section> {
             setMaxResults(PAGE_COUNT);
         }else{
             setMaxResults(getMaxResults() + PAGE_COUNT);
+            Logging.getLog(getClass()).debug("section moreResult count:" + getMaxResults());
         }
-    }
-
-    protected void setSql(){
-        String sql = "";
-        if ((getDistrictId() != null ) && !getDistrictId().trim().equals("")) {
-            sql += " where section.district.id = '" + getDistrictId().trim() + "' ";
-        }
-        if ((getSearchKey() != null) && !getSearchKey().trim().equals("")){
-            if ("".equals(sql)){
-                sql = " where ";
-            }else {
-                sql += " and ";
-            }
-
-            sql += "( (lower(section.pyCode) like lower(concat('%','" + getSearchKey().trim()
-                    + "','%')) ) or (lower(section.name) like lower(concat('%','" + getSearchKey().trim() +"','%'))))";
-        }
-        setEjbql(EJBQL + sql);
-        setMaxResults(PAGE_COUNT);
-
     }
 
     private String districtId;
@@ -67,7 +72,7 @@ public class SectionSelectList extends HouseEntityQuery<Section> {
 
     public void setDistrictId(String districtId) {
         this.districtId = districtId;
-        setSql();
+
     }
 
     public String getSearchKey() {
@@ -76,6 +81,11 @@ public class SectionSelectList extends HouseEntityQuery<Section> {
 
     public void setSearchKey(String searchKey) {
         this.searchKey = searchKey;
-        setSql();
+
+    }
+
+    @Override
+    protected String getPersistenceContextName() {
+        return "houseEntityManager";
     }
 }
