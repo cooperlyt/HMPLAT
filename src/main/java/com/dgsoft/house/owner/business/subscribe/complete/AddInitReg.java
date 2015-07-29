e6540b8d23deff8ecfc8e267d05075b28b4417df
Administrator <26982912@qq.com>
2015-07-29 15:33:13 +0800

    wxy

--- src/main/java/com/dgsoft/house/owner/business/subscribe/complete/AddInitReg.java
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/complete/AddInitReg.java
@@ -5,21 +5,19 @@ import com.dgsoft.house.HouseInfo;
 import com.dgsoft.house.owner.action.OwnerBusinessHome;
 import com.dgsoft.house.owner.model.HouseBusiness;
 import com.dgsoft.house.owner.model.HouseState;
-import org.apache.poi.ss.formula.functions.Now;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.faces.FacesMessages;
-import org.jboss.seam.international.StatusMessage;
-import org.jboss.seam.log.Logging;
 
 import java.util.Date;
 
 /**
- * Created by Administrator on 15-7-28.
- * 添加备案状态,
+ * Created by Administrator on 15-7-29.
+ * 添加初始登记
  */
-@Name("addContractsRecord")
-public class AddContractsRecord implements TaskCompleteSubscribeComponent {
+@Name("addInitReg")
+public class AddInitReg implements TaskCompleteSubscribeComponent {
+
 
     @In
     private OwnerBusinessHome ownerBusinessHome;
@@ -50,7 +48,7 @@ public class AddContractsRecord implements TaskCompleteSubscribeComponent {
 
 
         for (HouseBusiness houseBusiness:ownerBusinessHome.getInstance().getHouseBusinesses()){
-            HouseState state = new HouseState(houseBusiness.getAfterBusinessHouse(),HouseInfo.HouseStatus.CONTRACTS_RECORD,new Date());
+            HouseState state = new HouseState(houseBusiness.getAfterBusinessHouse(), HouseInfo.HouseStatus.INIT_REG,new Date());
             houseBusiness.getAfterBusinessHouse().getHouseStates().add(state);
 
         }
