c8f3dbe3800f395852830181e13ec2df3fb39243
Administrator <26982912@qq.com>
2015-07-30 14:06:15 +0800

    wxy

---
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/complete/MoveContractsRecord.java
@@ -0,0 +1,51 @@
+package com.dgsoft.house.owner.business.subscribe.complete;
+
+import com.dgsoft.common.system.business.TaskCompleteSubscribeComponent;
+import com.dgsoft.house.HouseInfo;
+import com.dgsoft.house.owner.action.OwnerBusinessHome;
+import com.dgsoft.house.owner.model.HouseBusiness;
+import com.dgsoft.house.owner.model.HouseState;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.log.Logging;
+import sun.rmi.runtime.Log;
+
+/**
+ * Created by Administrator on 15-7-29.
+ * 去掉一个备案状态
+ */
+@Name("moveContractsRecord")
+public class MoveContractsRecord implements TaskCompleteSubscribeComponent {
+
+
+    @In
+    private OwnerBusinessHome ownerBusinessHome;
+
+
+
+
+    @Override
+    public void valid() {
+
+    }
+
+    @Override
+    public boolean isPass() {
+        return true;
+    }
+
+    @Override
+    public void complete() {
+
+
+        for(HouseBusiness houseBusiness:ownerBusinessHome.getInstance().getHouseBusinesses()){
+            for(HouseState houseState:houseBusiness.getAfterBusinessHouse().getHouseStates()){
+                if(houseState.getState().equals(HouseInfo.HouseStatus.CONTRACTS_RECORD)){
+
+                    houseBusiness.getAfterBusinessHouse().getHouseStates().remove(houseState);
+                }
+            }
+
+        }
+    }
+}
