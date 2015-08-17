3eda306bb33a1884c23a55a74c5f7eff2c45c7f0
wxy <26982912@qq.com>
2015-08-17 18:35:53 +0800

    wxy

--- src/main/java/com/dgsoft/house/owner/business/subscribe/complete/AddContractsRecord.java
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/complete/AddContractsRecord.java
@@ -3,6 +3,7 @@ package com.dgsoft.house.owner.business.subscribe.complete;
 import com.dgsoft.common.system.business.TaskCompleteSubscribeComponent;
 import com.dgsoft.house.HouseInfo;
 import com.dgsoft.house.owner.action.OwnerBusinessHome;
+import com.dgsoft.house.owner.action.OwnerHouseHelper;
 import com.dgsoft.house.owner.model.AddHouseStatus;
 import com.dgsoft.house.owner.model.HouseBusiness;
 import org.apache.poi.ss.formula.functions.Now;
@@ -12,7 +13,10 @@ import org.jboss.seam.faces.FacesMessages;
 import org.jboss.seam.international.StatusMessage;
 import org.jboss.seam.log.Logging;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Administrator on 15-7-28.
@@ -51,6 +55,13 @@ public class AddContractsRecord implements TaskCompleteSubscribeComponent {
 
         for (HouseBusiness houseBusiness:ownerBusinessHome.getInstance().getHouseBusinesses()){
             houseBusiness.getAddHouseStatuses().add(new AddHouseStatus(HouseInfo.HouseStatus.CONTRACTS_RECORD,houseBusiness));
+
+            List<HouseInfo.HouseStatus> houseStatusList = new ArrayList<HouseInfo.HouseStatus>();
+            houseStatusList = OwnerHouseHelper.instance().getHouseAllStatus(houseBusiness.getHouseCode());
+            houseStatusList.add(HouseInfo.HouseStatus.CONTRACTS_RECORD);
+            Collections.sort(houseStatusList, new HouseInfo.StatusComparator());
+            houseBusiness.getAfterBusinessHouse().setMasterStatus(houseStatusList.get(0));
+
 
         }
 
