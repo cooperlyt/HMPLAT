5c64af1d4218fc74c36462b7074e03cebf426507
wxy <26982912@qq.com>
2015-09-28 16:46:04 +0800

    wxy

--- src/main/java/com/dgsoft/house/owner/business/subscribe/ContractOwnerSubscribe.java
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/ContractOwnerSubscribe.java
@@ -1,6 +1,7 @@
 package com.dgsoft.house.owner.business.subscribe;
 
 import com.dgsoft.common.system.PersonHelper;
+import com.dgsoft.house.SaleType;
 import com.dgsoft.house.owner.OwnerEntityHome;
 import com.dgsoft.house.owner.action.OwnerBusinessHome;
 import com.dgsoft.house.owner.model.ContractOwner;
@@ -33,7 +34,19 @@ public class ContractOwnerSubscribe extends OwnerEntityHome<ContractOwner> {
     public void checkHave(){
 
             setHave(true);
-        //}
+    }
+
+
+
+    private boolean type=true;
+
+
+    public boolean isType() {
+        return type;
+    }
+
+    public void setType(boolean type) {
+        this.type = type;
     }
 
     @Override
@@ -54,6 +67,11 @@ public class ContractOwnerSubscribe extends OwnerEntityHome<ContractOwner> {
             setId(ownerBusinessHome.getSingleHoues().getAfterBusinessHouse().getContractOwner().getId());
         }else{
             clearInstance();
+            if(type){
+                getInstance().setType(SaleType.MAP_SELL);
+            }else {
+                getInstance().setType(SaleType.NOW_SELL);
+            }
             ownerBusinessHome.getSingleHoues().getAfterBusinessHouse().setContractOwner(getInstance());
             getInstance().setHouseCode(ownerBusinessHome.getSingleHoues().getAfterBusinessHouse().getHouseCode());
         }
