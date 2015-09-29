3a0d4b9da61789e4189397e3e7a7b11ae10fb0dc
cooper <lyt3131@163.com>
2015-09-29 10:28:05 +0800

    Task Page complete

--- src/main/java/com/dgsoft/house/owner/business/BusinessSelectCreate.java
+++ src/main/java/com/dgsoft/house/owner/business/BusinessSelectCreate.java
@@ -3,23 +3,22 @@ package com.dgsoft.house.owner.business;
 import org.jboss.seam.annotations.Name;
 
 /**
- * Created by cooper on 8/11/15.
+ * Created by cooper on 9/29/15.
  */
-@Name("mulitHouseBusinessCreate")
-public class MulitOwnerBusinessCreate extends OwnerBusinessCreateComponent {
+@Name("businessSelectCreate")
+public class BusinessSelectCreate extends OwnerBusinessCreateComponent{
 
     private static final String NORMAL_BIZ_BEGIN_PAGE = "/business/houseOwner/MulitHouseStart.xhtml";
     private static final String PATCH_BIZ_BEGIN_PAGE = "/business/houseOwner/MulitHouseBusinessPatch.xthml";
 
+
     @Override
     protected String getNormalBusinessPage() {
-        return NORMAL_BIZ_BEGIN_PAGE;
+        return null;
     }
 
     @Override
     protected String getPatchBusinessPage() {
-        return PATCH_BIZ_BEGIN_PAGE;
+        return null;
     }
-
-
 }
