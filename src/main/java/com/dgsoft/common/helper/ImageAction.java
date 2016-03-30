e8ecb6541bd695f46e577da563831c5c5406ea8a
cooper <lyt3131@163.com>
2016-03-30 14:19:18 +0800

    project business search

---
+++ src/main/java/com/dgsoft/common/helper/ImageAction.java
@@ -0,0 +1,45 @@
+package com.dgsoft.common.helper;
+
+import com.dgsoft.common.system.RunParam;
+import org.jboss.seam.annotations.Name;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by cooper on 3/30/16.
+ */
+@Name("imageAction")
+public class ImageAction {
+
+
+
+
+    public ByteArrayInputStream getImage(String fileId){
+
+        try {
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            URL url = new URL(RunParam.instance().getStringParamValue("IMG_SERVER_ADDRESS") + "img/orig/" + fileId);
+
+            url.openConnection().connect();
+
+            BufferedImage image = ImageIO.read(url.openStream());
+
+            outStream.close();
+            ImageIO.write(image,"JPEG",outStream);
+            return new ByteArrayInputStream(outStream.toByteArray());
+
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("url is fail");
+        } catch (IOException e) {
+
+            throw new IllegalArgumentException("url is fail");
+        }
+    }
+
+}
