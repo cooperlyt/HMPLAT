4e60b5b6b609fc71a915cda85a6d0c8a03c298cb
wxy <26982912@qq.com>
2016-04-02 12:40:48 +0800

    wxy

---
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/BusinessMulitHouseOwnerSubscribe.java
@@ -0,0 +1,105 @@
+package com.dgsoft.house.owner.business.subscribe;
+
+import com.dgsoft.common.system.PersonEntity;
+import com.dgsoft.common.system.business.TaskSubscribeComponent;
+import com.dgsoft.house.owner.HouseOwnerPersonHelper;
+import com.dgsoft.house.owner.OwnerEntityHome;
+import com.dgsoft.house.owner.action.OwnerBusinessHome;
+import com.dgsoft.house.owner.model.BusinessHouse;
+import com.dgsoft.house.owner.model.BusinessHouseOwner;
+import com.dgsoft.house.owner.model.HouseBusiness;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+/**
+ * Created by wxy on 2016-04-01.
+ */
+@Name("businessMulitHouseOwnerSubscribe")
+@Scope(ScopeType.CONVERSATION)
+public class BusinessMulitHouseOwnerSubscribe implements TaskSubscribeComponent {
+
+
+    @In
+    private OwnerBusinessHome ownerBusinessHome;
+
+
+    private BusinessHouseOwner businessHouseOwner = new BusinessHouseOwner();
+
+
+
+
+   @Create
+   public void initHouseOwner(){
+       if (ownerBusinessHome.getInstance().getHouseBusinesses()!=null
+               && ownerBusinessHome.getInstance().getHouseBusinesses().size()>0){
+
+           if (ownerBusinessHome.getInstance().getHouseBusinesses().iterator().next().getAfterBusinessHouse().getBusinessHouseOwner()== null) {
+
+               BusinessHouse businessHouse = ownerBusinessHome.getInstance().getHouseBusinesses().iterator().next().getStartBusinessHouse();
+               businessHouseOwner.setPersonName(businessHouse.getDeveloperName());
+
+               businessHouseOwner.setCredentialsType(PersonEntity.CredentialsType.OTHER);
+               if (businessHouse.getDeveloperCode() != null
+                       && !businessHouse.getDeveloperCode().equals("")) {
+                   businessHouseOwner.setCredentialsNumber(businessHouse.getDeveloperCode());
+               }
+           }else{
+               businessHouseOwner  = ownerBusinessHome.getInstance().getHouseBusinesses().iterator().next().getAfterBusinessHouse().getBusinessHouseOwner();
+           }
+
+           businessHouseOwner.setOwnerBusiness(ownerBusinessHome.getInstance());
+
+       }
+
+   }
+
+
+
+    @Override
+    public void initSubscribe() {
+
+
+
+
+    }
+
+    @Override
+    public void validSubscribe() {
+
+    }
+
+    @Override
+    public boolean isPass() {
+        if (ownerBusinessHome.getInstance().getHouseBusinesses()!=null
+                && ownerBusinessHome.getInstance().getHouseBusinesses().size()>0) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public boolean saveSubscribe() {
+        for (HouseBusiness houseBusiness:ownerBusinessHome.getInstance().getHouseBusinesses()){
+            BusinessHouseOwner houseOwner = new BusinessHouseOwner(ownerBusinessHome.getInstance(),businessHouseOwner);
+            houseBusiness.getAfterBusinessHouse().setBusinessHouseOwner(houseOwner);
+            houseBusiness.getAfterBusinessHouse().setOldOwner(houseOwner);
+        }
+        return true;
+
+    }
+
+    private HouseOwnerPersonHelper personHelper;
+
+    public HouseOwnerPersonHelper getPersonInstance() {
+        if ((personHelper == null) ||
+                (personHelper.getPersonEntity() != businessHouseOwner)) {
+            personHelper = new HouseOwnerPersonHelper
+                    (businessHouseOwner);
+        }
+        return personHelper;
+    }
+}
