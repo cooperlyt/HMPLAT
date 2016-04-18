4b5cd382fa3b990d1c2a410c12ca2574ea2f5f10
wxy <26982912@qq.com>
2016-04-18 17:34:28 +0800

    wxy

--- design/database/Update_1.2.0.sql
+++ design/database/Update_1.2.0.sql
@@ -138,7 +138,7 @@ insert FEE (ID,CATEGORY,NAME,DESCRIPTION,FOR_EACH_VALUES,FOR_EACH_VAR,FEE_EL,DET
  value ('REGISTER.HB','REGISTER','登记费','','#{ownerBusinessHome.instance.houseBusinesses}','_houses','#{_houses.afterBusinessHouse.useType==''80''?160.0:1100.0}','#{_houses.afterBusinessHouse.useType==''80'' ? ''160'' : ''1100''}',14);
 insert BUSINESS_AND_FEE (BUSINESS,FEE) values ('WP150','REGISTER.HB');
 
-
+insert DB_PLAT_SYSTEM.SYSTEM_PARAM (ID, TYPE, VALUE, MEMO) values ('CreateCradNumberType','INTEGER','1','1:生成的权证号带-;2:生成的权证号不带-');
 delete from VIEW_SUBSCRIBE where REG_NAME='RecordStoreHouseView';
 delete from VIEW_SUBSCRIBE where REG_NAME='CheckEmpRecordEdit';
 DELETE FROM TASK_SUBSCRIBE WHERE REG_NAME='RecordCoverPrint';
