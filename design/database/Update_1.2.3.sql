9b5f7deebc782563f2de6def14d47e1b2ea90357
cooper <lyt3131@163.com>
2016-08-06 18:00:02 +0800

    lll

---
+++ design/database/Update_1.2.3.sql
@@ -0,0 +1,2 @@
+INSERT INTO DB_PLAT_SYSTEM.SYSTEM_PARAM(ID,TYPE,VALUE,MEMO) VALUES('idleGirdMapUnitSort','STRING','desc','自动分户图单元排序方式');
+
---
+++ src/main/java/com/dgsoft/house/AutoGridMapComparator.java
@@ -0,0 +1,62 @@
+package com.dgsoft.house;
+
+import com.dgsoft.common.system.RunParam;
+
+import java.util.Comparator;
+
+/**
+ * Created by cooper on 8/6/16.
+ */
+public class AutoGridMapComparator {
+
+    public static Comparator<String> getUnitComparator(){
+       String param = RunParam.instance().getStringParamValue("idleGirdMapUnitSort");
+        if (param.equals("desc")){
+            return new DescUnitComparator();
+        }else {
+            return new DefaultUnitComparator();
+        }
+    }
+
+    public static Comparator<String> getFloorComparator(){
+        return new DefaultFloorComparator();
+    }
+
+    public static Comparator<HouseInfo> getHouseComparator(){
+        return new DefaultHouseComparator();
+    }
+
+    public static class DefaultUnitComparator implements Comparator<String>{
+
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.compareTo(o2);
+        }
+    }
+
+    public static class DescUnitComparator implements Comparator<String>{
+
+        @Override
+        public int compare(String o1, String o2) {
+            return o2.compareTo(o1);
+        }
+    }
+
+    public static class DefaultFloorComparator implements Comparator<String>{
+
+        @Override
+        public int compare(String o1, String o2) {
+            return o2.compareTo(o1);
+        }
+    }
+
+    public static class DefaultHouseComparator implements Comparator<HouseInfo>{
+
+
+        @Override
+        public int compare(HouseInfo o1, HouseInfo o2) {
+            return o1.getHouseOrder().compareTo(o2.getHouseOrder());
+        }
+    }
+
+}
