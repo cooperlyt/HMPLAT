d96d62efc08c618e10d28841622a846729667d29
cooper <lyt3131@163.com>
2016-08-09 20:29:08 +0800

    lll

--- design/database/Update_1.2.3.sql
+++ design/database/Update_1.2.3.sql
@@ -8,3 +8,6 @@ INSERT INTO DB_PLAT_SYSTEM.SYSTEM_PARAM(ID,TYPE,VALUE,MEMO) VALUES('applyPersonT
 
 INSERT INTO FUNCTION (ID, NAME, CATEGORY, ICON, LOCATION, BANNER, PRIORITY, MEMO,NEED_CONVERSATION) VALUES ('owner.todayBusinessList', '当日业务列表', 'DAY_WORK', '', '/func/house/owner/TodayBusinessList.xhtml', '', '5', '',b'0');
 
+insert SYSTEM_PARAM (ID, TYPE, VALUE, MEMO)
+  value ('MortgagePrintOwnerCard','INTEGER','2','1-他项权证打印所有权证号只没有共有权证号;2-他项权证打印所有权证带共有权证号');
+
--- design/database/Update_1.2.4.sql
+++
@@ -1,2 +0,0 @@
-insert SYSTEM_PARAM (ID, TYPE, VALUE, MEMO)
-  value ('MortgagePrintOwnerCard','INTEGER','1','1-他项权证打印所有权证号只没有共有权证号;2-他项权证打印所有权证带共有权证号');
