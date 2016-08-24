b9ff335992a469f973e36b297dce0e1a0042d3ba
wxy <26982912@qq.com>
2016-08-24 20:28:07 +0800

    wxy

---
+++ design/database/Update_1.2.4.sql
@@ -0,0 +1,2 @@
+﻿update DB_PLAT_SYSTEM.REPORT set PAGE='/report/msFcc/MsApplyShifReisterCommercialBook.xhtml' where ID='36';
+update DB_PLAT_SYSTEM.REPORT set PAGE='/report/msFcc/MsApplyShifReisterBook.xhtml' where ID='4';
