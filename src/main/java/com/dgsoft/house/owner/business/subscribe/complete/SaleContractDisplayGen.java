8c0b27311f94a14818b107bbe9c9af24e08ba646
wxy <26982912@qq.com>
2016-12-11 15:33:09 +0800

    wxy

--- src/main/java/com/dgsoft/house/owner/business/subscribe/complete/SaleContractDisplayGen.java
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/complete/SaleContractDisplayGen.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 /**
  * Created by cooper on 20/11/2016.
+ * 房屋业务查询
  */
 @Name("saleContractDisplayGen")
 public class SaleContractDisplayGen implements TaskCompleteSubscribeComponent {
