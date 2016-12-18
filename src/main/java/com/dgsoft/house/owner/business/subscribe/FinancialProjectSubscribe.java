f15cc50218f814c84882775702ef3cb261713a39
wxy <26982912@qq.com>
2016-12-18 11:16:36 +0800

    wxy

---
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/FinancialProjectSubscribe.java
@@ -0,0 +1,14 @@
+package com.dgsoft.house.owner.business.subscribe;
+
+import org.jboss.seam.annotations.Name;
+
+/**
+ * Created by wxy on 2016-12-18.
+ */
+@Name("FinancialProjectSubscribe")
+public class FinancialProjectSubscribe extends FinancialBaseSubscribe {
+    @Override
+    protected void addMortgage() {
+
+    }
+}
