18fec7453c419ad00da734abdc69ded6cb06ffe0
wxy <26982912@qq.com>
2017-05-16 16:56:19 +0800

    wxy

---
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/complete/NoticeDisplayGen.java
@@ -0,0 +1,86 @@
+package com.dgsoft.house.owner.business.subscribe.complete;
+
+import com.dgsoft.common.system.business.TaskCompleteSubscribeComponent;
+import com.dgsoft.house.DescriptionDisplay;
+import com.dgsoft.house.owner.action.OwnerBusinessHome;
+import com.dgsoft.house.owner.model.HouseBusiness;
+import com.dgsoft.house.owner.model.HouseContract;
+import com.dgsoft.house.owner.model.PowerPerson;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+/**
+ * Created by wxy on 2017-05-16.
+ */
+@Name("noticeDisplayGen")
+public class NoticeDisplayGen implements TaskCompleteSubscribeComponent {
+    @In
+    private OwnerBusinessHome ownerBusinessHome;
+
+    @Override
+    public void valid() {
+
+    }
+
+    @Override
+    public boolean isPass() {
+        return true;
+    }
+
+    @Override
+    public void complete() {
+
+        for(HouseBusiness bh: ownerBusinessHome.getInstance().getHouseBusinesses()){
+
+            DescriptionDisplay businessDisplay = new DescriptionDisplay();
+
+            businessDisplay.newLine(DescriptionDisplay.DisplayStyle.NORMAL);
+
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.LABEL, "房屋编号");
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.PARAGRAPH,bh.getHouseCode());
+
+            HouseContract houseContract = bh.getAfterBusinessHouse().getSaleContract();
+
+            if (houseContract == null){
+                houseContract = bh.getStartBusinessHouse().getSaleContract();
+            }
+
+            if (houseContract != null) {
+                businessDisplay.addData(DescriptionDisplay.DisplayStyle.LABEL, "合同编号");
+                businessDisplay.addData(DescriptionDisplay.DisplayStyle.PARAGRAPH,houseContract.getContractNumber());
+            }
+
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.LABEL, "预告人");
+
+            String contractPersonNames = "";
+            for (PowerPerson pp: bh.getAfterBusinessHouse().getAllNewPowerPersonList()){
+                if (pp.getType().equals(PowerPerson.PowerPersonType.OWNER)){
+                    if (!"".equals(contractPersonNames)){
+                        contractPersonNames += ",";
+                    }
+                    contractPersonNames += pp.getPersonName();
+                }
+            }
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.PARAGRAPH, contractPersonNames);
+
+            businessDisplay.newLine(DescriptionDisplay.DisplayStyle.NORMAL);
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.PARAGRAPH,bh.getAfterBusinessHouse().getAddress());
+
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.IMPORTANT,bh.getAfterBusinessHouse().getMapNumber());
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.DECORATE,"图");
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.IMPORTANT,bh.getAfterBusinessHouse().getBlockNo());
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.DECORATE,"丘");
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.IMPORTANT,bh.getAfterBusinessHouse().getBuildNo());
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.DECORATE,"幢");
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.IMPORTANT,bh.getAfterBusinessHouse().getHouseOrder());
+            businessDisplay.addData(DescriptionDisplay.DisplayStyle.DECORATE,"房");
+
+
+            bh.setDisplay(DescriptionDisplay.toStringValue(businessDisplay));
+
+        }
+
+
+
+    }
+}
