111faad37aa64b563407f43c3647bb0ef6fe4fe6
wxy <26982912@qq.com>
2017-06-13 21:07:56 +0800

    wxy

---
+++ src/main/java/com/dgsoft/house/owner/business/create/ContractPersonFill.java
@@ -0,0 +1,34 @@
+package com.dgsoft.house.owner.business.create;
+
+import com.dgsoft.common.system.business.BusinessDataFill;
+import com.dgsoft.house.owner.action.OwnerBusinessHome;
+import com.dgsoft.house.owner.model.HouseBusiness;
+import com.dgsoft.house.owner.model.PowerPerson;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.log.Logging;
+import sun.font.TrueTypeFont;
+
+/**
+ * Created by wxy on 2017-06-13.
+ */
+@Name("contractPersonFill")
+public class ContractPersonFill implements BusinessDataFill {
+    @In
+    private OwnerBusinessHome ownerBusinessHome;
+    @Override
+    public void fillData() {
+
+
+
+            for(PowerPerson pp: ownerBusinessHome.getInstance().getSelectBusiness().getSingleHoues().getAfterBusinessHouse().getPowerPersons()){
+
+                if (!pp.isOld() && pp.getType().equals(PowerPerson.PowerPersonType.CONTRACT) ){
+
+                    ownerBusinessHome.getInstance().getSingleHoues().getAfterBusinessHouse().getPowerPersons().add(new PowerPerson(PowerPerson.PowerPersonType.CONTRACT, true, pp, pp.getPriority()));
+                }
+            }
+
+
+    }
+}
