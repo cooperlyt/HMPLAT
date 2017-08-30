23fdecd90ad4ca63e5b77b2695f3f0a42c65b2e9
wxy <26982912@qq.com>
2017-08-30 17:59:15 +0800

    wxy

---
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/complete/AddSceondCheckEmp.java
@@ -0,0 +1,60 @@
+package com.dgsoft.house.owner.business.subscribe.complete;
+
+import com.dgsoft.common.system.AuthenticationInfo;
+import com.dgsoft.common.system.business.TaskCompleteSubscribeComponent;
+import com.dgsoft.house.owner.action.OwnerBusinessHome;
+import com.dgsoft.house.owner.model.BusinessEmp;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+
+import java.util.Date;
+
+/**
+ * Created by wxy on 2017-08-30.
+ */
+@Name("addSceondCheckEmp")
+public class AddSceondCheckEmp implements TaskCompleteSubscribeComponent {
+
+
+    @In
+    private OwnerBusinessHome ownerBusinessHome;
+
+    @In
+    private AuthenticationInfo authInfo;
+
+    @In(required = false,scope = ScopeType.BUSINESS_PROCESS)
+    private String transitionComments;
+
+    @Override
+    public void valid() {
+
+    }
+
+    @Override
+    public boolean isPass() {
+        return true;
+    }
+
+    @Override
+    public void complete() {
+
+        for (BusinessEmp businessEmp:ownerBusinessHome.getInstance().getBusinessEmps()){
+            if (businessEmp.getType().equals(BusinessEmp.EmpType.SECOND_CHECK)){
+                ownerBusinessHome.getInstance().getBusinessEmps().remove(businessEmp);
+                break;
+            }
+
+
+        }
+        BusinessEmp businessEmp = new BusinessEmp(BusinessEmp.EmpType.SECOND_CHECK);
+        businessEmp.setEmpName(authInfo.getLoginEmployee().getPersonName());
+        businessEmp.setEmpCode(authInfo.getLoginEmployee().getId());
+        businessEmp.setOperDate(new Date());
+        businessEmp.setComments(transitionComments);
+        businessEmp.setOwnerBusiness(ownerBusinessHome.getInstance());
+        ownerBusinessHome.getInstance().setCheckTime(businessEmp.getOperDate());
+        ownerBusinessHome.getInstance().getBusinessEmps().add(businessEmp);
+
+    }
+}
