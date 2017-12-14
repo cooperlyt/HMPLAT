fb3108baf6f11dc1585801232bf7ed96bbe67cc2
wxy <26982912@qq.com>
2017-12-14 20:39:53 +0800

    wxy

--- src/main/java/com/dgsoft/house/owner/model/Reason.java
+++ src/main/java/com/dgsoft/house/owner/model/Reason.java
@@ -74,8 +74,8 @@ public class Reason implements java.io.Serializable {
         this.type = type;
     }
 
-    @Column(name = "REASON", nullable = true, length = 200)
-    @Size(max = 200)
+    @Column(name = "REASON", nullable = true, length = 400)
+    @Size(max = 400)
     public String getReason() {
         return this.reason;
     }
