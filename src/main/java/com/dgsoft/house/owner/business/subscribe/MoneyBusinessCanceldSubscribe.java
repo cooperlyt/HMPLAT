9a75677b36847f8b6dee0f4592d7fb537591bcaf
wxy <26982912@qq.com>
2018-04-24 00:13:24 +0800

    wxy

--- src/main/java/com/dgsoft/house/owner/business/subscribe/MoneyBusinessCanceldSubscribe.java
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/MoneyBusinessCanceldSubscribe.java
@@ -1,18 +1,18 @@
 package com.dgsoft.house.owner.business.subscribe;
 
-import com.dgsoft.common.system.business.TaskSubscribeComponent;
+import com.dgsoft.common.system.business.BusinessInstance;
 import com.dgsoft.house.owner.OwnerEntityHome;
 import com.dgsoft.house.owner.action.OwnerBusinessHome;
 import com.dgsoft.house.owner.model.MoneyBusiness;
-import com.dgsoft.house.owner.model.MoneyPayInfo;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 /**
  * Created by wxy on 2018-04-23.
+ * 资金监管 撤回 取消，交易没成功，
  */
-@Name("moneyBusinessChangedSubscribe")
-public class MoneyBusinessChangedSubscribe extends OwnerEntityHome<MoneyBusiness> {
+@Name("moneyBusinessCanceldSubscribe")
+public class MoneyBusinessCanceldSubscribe extends OwnerEntityHome<MoneyBusiness> {
 
     @In
     private OwnerBusinessHome ownerBusinessHome;
@@ -20,16 +20,17 @@ public class MoneyBusinessChangedSubscribe extends OwnerEntityHome<MoneyBusiness
     @Override
     public MoneyBusiness createInstance(){
         if (ownerBusinessHome.getInstance().getMoneyBusinesses().isEmpty()) {
-            MoneyBusiness moneyBusiness = new MoneyBusiness(ownerBusinessHome.getInstance(), ownerBusinessHome.getInstance().getSelectBusiness().getMoneyBusiness(), 1);
+            MoneyBusiness moneyBusiness = new MoneyBusiness(ownerBusinessHome.getInstance(), ownerBusinessHome.getInstance().getSelectBusiness().getMoneyBusiness(), 2);
+            moneyBusiness.setStatus(MoneyBusiness.MoneyBusinessStatus.CANCEL);
             ownerBusinessHome.getInstance().getMoneyBusinesses().add(moneyBusiness);
             for (MoneyBusiness omb : ownerBusinessHome.getInstance().getSelectBusiness().getMoneyBusinesses()) {
                 omb.setStatus(MoneyBusiness.MoneyBusinessStatus.CHANGED);
             }
-            return  moneyBusiness;
+            return moneyBusiness;
         }else{
             return ownerBusinessHome.getInstance().getMoneyBusiness();
-        }
 
+        }
 
     }
 
--- src/main/java/com/dgsoft/house/owner/business/subscribe/MoneyBusinessRemovedSubscribe.java
+++ src/main/java/com/dgsoft/house/owner/business/subscribe/MoneyBusinessRemovedSubscribe.java
@@ -1,19 +1,17 @@
 package com.dgsoft.house.owner.business.subscribe;
 
-import com.dgsoft.common.system.business.TaskSubscribeComponent;
 import com.dgsoft.house.owner.OwnerEntityHome;
 import com.dgsoft.house.owner.action.OwnerBusinessHome;
 import com.dgsoft.house.owner.model.MoneyBusiness;
-import com.dgsoft.house.owner.model.MoneyPayInfo;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 /**
  * Created by wxy on 2018-04-23.
+ * 交易完成后解除资金监管
  */
-@Name("moneyBusinessChangedSubscribe")
-public class MoneyBusinessChangedSubscribe extends OwnerEntityHome<MoneyBusiness> {
-
+@Name("moneyBusinessRemovedSubscribe")
+public class MoneyBusinessRemovedSubscribe extends OwnerEntityHome<MoneyBusiness> {
     @In
     private OwnerBusinessHome ownerBusinessHome;
 
@@ -21,6 +19,7 @@ public class MoneyBusinessChangedSubscribe extends OwnerEntityHome<MoneyBusiness
     public MoneyBusiness createInstance(){
         if (ownerBusinessHome.getInstance().getMoneyBusinesses().isEmpty()) {
             MoneyBusiness moneyBusiness = new MoneyBusiness(ownerBusinessHome.getInstance(), ownerBusinessHome.getInstance().getSelectBusiness().getMoneyBusiness(), 1);
+            moneyBusiness.setStatus(MoneyBusiness.MoneyBusinessStatus.REGISTERED);
             ownerBusinessHome.getInstance().getMoneyBusinesses().add(moneyBusiness);
             for (MoneyBusiness omb : ownerBusinessHome.getInstance().getSelectBusiness().getMoneyBusinesses()) {
                 omb.setStatus(MoneyBusiness.MoneyBusinessStatus.CHANGED);
@@ -32,5 +31,4 @@ public class MoneyBusinessChangedSubscribe extends OwnerEntityHome<MoneyBusiness
 
 
     }
-
 }
