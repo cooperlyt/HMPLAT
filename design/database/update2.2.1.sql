c339639aca2e1064ee4c09781658250db0a94e68
wxy <26982912@qq.com>
2018-04-24 20:32:00 +0800

    wxy

--- design/database/update2.2.1.sql
+++ design/database/update2.2.1.sql
@@ -168,6 +168,8 @@ INSERT DB_PLAT_SYSTEM.FUNCTION (ID, NAME, ICON, LOCATION, BANNER, PRIORITY, MEMO
   VALUE ('money.businessSearch','资金监管业务查询','','/func/house/owner/MoneyBusinessSearch.xhtml',true,4,'','DAY_WORK',0);
 INSERT DB_PLAT_SYSTEM.ROLE_FUNCTION(FUN_ID, ROL_ID) VALUE ('money.businessSearch','cqsl');
 
+INSERT DB_PLAT_SYSTEM.REPORT (ID, NAME, DESCRIPTION, PAGE) VALUE ('42','资金监管审批表','资金监管审批表','/report/dgfcc/MoneyBusinessChangeReisterBook.xhtml');
+INSERT DB_PLAT_SYSTEM.REPORT (ID, NAME, DESCRIPTION, PAGE) VALUE ('43','资金监管告知单','资金监管告知单','/report/dgfcc/MoneyBusinessChangeTicket.xhtml');
 
 
 
