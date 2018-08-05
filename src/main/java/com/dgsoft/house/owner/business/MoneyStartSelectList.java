8cd301690a520856acb827eecd9a52bc7f0ff0b7
wxy <26982912@qq.com>
2018-08-05 22:45:17 +0800

    wxy

--- src/main/java/com/dgsoft/house/owner/business/MoneyStartSelectList.java
+++ src/main/java/com/dgsoft/house/owner/business/MoneyStartSelectList.java
@@ -7,6 +7,7 @@ import com.dgsoft.house.owner.action.HouseBusinessSearch;
 import com.dgsoft.house.owner.action.MoneyBusinessCondition;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.log.Logging;
 
 /**
  * Created by wxy on 2018-04-18.
@@ -51,8 +52,10 @@ public class MoneyStartSelectList extends HouseBusinessSearch {
 
         defineIdCondition += ")";
         if (moneyBusinessCondition.getSearchType().equals(MoneyBusinessCondition.SearchType.HOUSE_OWNER) || moneyBusinessCondition.getSearchType().equals(MoneyBusinessCondition.SearchType.PERSON)){
+            Logging.getLog(getClass()).debug("22222--业务查询");
             return result + " where owner.old = false and ob.defineId in " + defineIdCondition + " and ob.status <> 'RUNNING' and ob.status <> 'MODIFYING' and ob.status <> 'SUSPEND' and ob.type in ('NORMAL_BIZ','MODIFY_BIZ') and mb.status<>'CHANGED'";
         }else{
+            Logging.getLog(getClass()).debug("111111--受理查询");
             return result + " where ob.defineId in " + defineIdCondition + " and ob.status <> 'RUNNING' and ob.status <> 'MODIFYING' and ob.status <> 'SUSPEND' and ob.type in ('NORMAL_BIZ','MODIFY_BIZ') and mb.status<>'CHANGED'";
         }
 
