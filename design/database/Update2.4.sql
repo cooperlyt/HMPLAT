e3a29892b6c750600ff2f27b0603ee38ba762b8f
wxy <26982912@qq.com>
2019-08-19 22:44:43 +0800

    wxy

---
+++ design/database/Update2.4.sql
@@ -0,0 +1,2 @@
+﻿INSERT DB_PLAT_SYSTEM.REPORT (ID, NAME, DESCRIPTION, PAGE) VALUE ('93','商品房预售监管资金网签未备案退款审批单','商品房预售监管资金网签未备案退款审批单','/report/dgfcc/DgMoneyBackAbortReisterBook.xhtml');
+INSERT DB_PLAT_SYSTEM.REPORT (ID, NAME, DESCRIPTION, PAGE) VALUE ('94','商品房预售监管资金网签未备案退款申请表','商品房预售监管资金网签未备案退款申请表','/report/dgfcc/DgMoneyBackAbortTicket.xhtml');
