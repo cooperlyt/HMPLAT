95a1a1ec8f4503b7535a237fdea9f3abd9055380
wxy <26982912@qq.com>
2019-08-20 10:59:08 +0800

    wxy

---
+++ src/main/java/com/dgsoft/house/owner/business/validation/BusinessHouseOnlyDeleteMoneyBack.java
@@ -0,0 +1,36 @@
+package com.dgsoft.house.owner.business.validation;
+
+import com.dgsoft.common.system.business.BusinessDataValid;
+import com.dgsoft.house.owner.OwnerEntityLoader;
+import com.dgsoft.house.owner.model.HouseBusiness;
+import com.dgsoft.house.owner.model.MoneyBackBusiness;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+import java.util.List;
+
+/**
+ * Created by wxy on 2019-08-20.
+ */
+@Name("businessHouseOnlyDeleteMoneyBack")
+@Scope(ScopeType.STATELESS)
+public class BusinessHouseOnlyDeleteMoneyBack extends HouseBusinessValid  {
+
+    @In(create = true)
+    private OwnerEntityLoader ownerEntityLoader;
+
+    @Override
+    public ValidResult valid(HouseBusiness houseBusiness) {
+
+        List<MoneyBackBusiness> moneyBackBusinessList = ownerEntityLoader.getEntityManager().createQuery("select mb from MoneyBackBusiness mb where mb.ownerBusiness.status in('COMPLETE','RUNNING','SUSPEND') and mb.ownerBusiness.type<>'CANCEL_BIZ' and mb.contract=:contract and mc.backType in ('','')", MoneyBackBusiness.class)
+                .setParameter("contract", houseBusiness.getHouseContract().getContractNumber()).getResultList();
+
+        if (moneyBackBusinessList ==null || moneyBackBusinessList.size()<=0){
+            return new ValidResult(ValidResultLevel.SUCCESS);
+
+        }
+        return new ValidResult("house_have_moneyBackBusiness", ValidResultLevel.ERROR, moneyBackBusinessList.get(0).getOwnerBusiness().getId(),houseBusiness.getHouseContract().getContractNumber());
+    }
+}
